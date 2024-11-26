cc6fbcc10d266d40bd8bbaa8dec0385a65ea2aab
Kenji Troy D. Daymiel <kenjiddaymiel@su.edu.ph>
2024-11-26 12:17:39 +0000

    da

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pytesseract
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test', methods=['POST'])
 def ocr():
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
     return pytesseract.image_to_string(Image.open('test.png'))
